bc4a37f78890f41324c0bbead3334c1756145bb4
Sabarinathan Arumugam <126689513+Sabari-Dev@users.noreply.github.com>
2023-08-31 16:26:17 +0530

    Update Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,17 +1,30 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
 import Button from "react-bootstrap/Button";
 import { Link, useNavigate } from "react-router-dom";
 import { auth, db } from "../Config/Config";
-import { updateDoc, doc } from "firebase/firestore";
+import { updateDoc, doc, getDoc } from "firebase/firestore";
 import { signOut } from "firebase/auth";
+import Image from "react-bootstrap/Image";
 import { AuthContext } from "../context/auth";
 import logo from "../logo.png";
+import profileImg from "../profile.jpg";
 
 const Nav = () => {
   const { user } = useContext(AuthContext);
   let navigate = useNavigate();
+  const [userProfile, setUserProfile] = useState("");
+
+  useEffect(() => {
+    const getProfile = async () => {
+      const docRef = doc(db, "Users", auth.currentUser.uid);
+      const docSnap = await getDoc(docRef);
+      // console.log(docSnap.data());
+      setUserProfile(docSnap.data().avatar);
+    };
+    getProfile();
+  }, [userProfile]);
   const handleSignOut = async () => {
     await updateDoc(doc(db, "Users", auth.currentUser.uid), {
       isOnline: false,
@@ -20,10 +33,10 @@ const Nav = () => {
     navigate("/signIn");
   };
   return (
-    <Navbar className="bg-body-tertiary p-0">
+    <Navbar className="bg-body-tertiary p-0 navbar">
       <Container>
         <Navbar.Brand className="p-0">
-          <Link to="/">
+          <Link to={user ? "/home" : "/"}>
             <img
               src={logo}
               alt="logo"
@@ -38,6 +51,13 @@ const Nav = () => {
             <>
               <Link to="/profile">
                 <Button variant="primary" className="mx-2 px-3">
+                  <Image
+                    src={userProfile ? userProfile : profileImg}
+                    height={"25px"}
+                    width={"25px"}
+                    roundedCircle
+                    className="me-2"
+                  />
                   Profile
                 </Button>
               </Link>
